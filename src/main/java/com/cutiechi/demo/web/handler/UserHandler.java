48e1f2042fb488fd59f5c851446d5add46e1ea15
cutiechi <chi@cutie.ooo>
2018-06-07 08:45:37 +0800

    feat(handler): add user handler

---
+++ src/main/java/com/cutiechi/demo/web/handler/UserHandler.java
@@ -0,0 +1,13 @@
+package com.cutiechi.demo.web.handler;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 用户处理类
+ *
+ * @author Cutie Chi
+ */
+@Component
+public final class UserHandler {
+
+}
