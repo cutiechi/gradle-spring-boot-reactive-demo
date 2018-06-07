34fead07f9343f7353e793192079a9337b19aa0c
cutiechi <chi@cutie.ooo>
2018-06-07 08:44:19 +0800

    feat(service): add list all user method

--- src/main/java/com/cutiechi/demo/service/UserService.java
+++ src/main/java/com/cutiechi/demo/service/UserService.java
@@ -1,5 +1,8 @@
 package com.cutiechi.demo.service;
 
+import com.cutiechi.demo.model.entity.User;
+import reactor.core.publisher.Flux;
+
 /**
  * 用户业务逻辑接口
  *
@@ -7,4 +10,10 @@ package com.cutiechi.demo.service;
  */
 public interface UserService {
 
+    /**
+     * 获取全部用户列表
+     *
+     * @return userFlux
+     */
+    Flux<User> listAll ();
 }
--- src/main/java/com/cutiechi/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cutiechi/demo/service/impl/UserServiceImpl.java
@@ -1,8 +1,13 @@
 package com.cutiechi.demo.service.impl;
 
+import com.cutiechi.demo.model.entity.User;
+import com.cutiechi.demo.repository.UserRepository;
 import com.cutiechi.demo.service.UserService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import reactor.core.publisher.Flux;
 
 /**
  * 用户业务逻辑实现类
@@ -12,4 +17,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl implements UserService {
 
+    /**
+     * 用户数据仓库对象
+     */
+    private final UserRepository userRepository;
+
+    /**
+     * Spring 自动注入
+     *
+     * @param userRepository 用户数据仓库对象
+     */
+    @Autowired
+    public UserServiceImpl (UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    /**
+     * 获取全部用户列表
+     *
+     * @return userFlux
+     */
+    @Override
+    public Flux<User> listAll () {
+        return userRepository.findAll();
+    }
 }
