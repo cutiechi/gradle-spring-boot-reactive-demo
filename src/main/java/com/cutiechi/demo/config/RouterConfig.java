466a81e90216428e783d692418cd6c7f5e36f62a
cutiechi <chi@cutie.ooo>
2018-06-07 09:05:05 +0800

    feat(config): add router config class

---
+++ src/main/java/com/cutiechi/demo/config/RouterConfig.java
@@ -0,0 +1,13 @@
+package com.cutiechi.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 路由配置类
+ *
+ * @author Cutie Chi
+ */
+@Configuration
+public class RouterConfig {
+
+}
