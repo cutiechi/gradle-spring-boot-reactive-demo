fcbcb4fc75e1d62131020a69fe76498a7c1f5430
cutiechi <chi@cutie.ooo>
2018-06-07 08:11:00 +0800

    initial commit

---
+++ src/main/java/com/cutiechi/demo/GradleSpringBootReactiveDemoApplication.java
@@ -0,0 +1,12 @@
+package com.cutiechi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSpringBootReactiveDemoApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(GradleSpringBootReactiveDemoApplication.class, args);
+    }
+}
