113c613468614c18b296509139f78dd067bd6dbd
cutiechi <chi@cutie.ooo>
2018-06-07 08:49:21 +0800

    feat(handler): add list all user method

--- src/main/java/com/cutiechi/demo/web/handler/UserHandler.java
+++ src/main/java/com/cutiechi/demo/web/handler/UserHandler.java
@@ -1,6 +1,14 @@
 package com.cutiechi.demo.web.handler;
 
+import com.cutiechi.demo.model.entity.User;
+import com.cutiechi.demo.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import reactor.core.publisher.Mono;
 
 /**
  * 用户处理类
@@ -10,4 +18,30 @@ import org.springframework.stereotype.Component;
 @Component
 public final class UserHandler {
 
+    /**
+     * 用户业务逻辑对象
+     */
+    private final UserService userService;
+
+    /**
+     * Spring 自动注入
+     *
+     * @param userService 用户业务逻辑对象
+     */
+    @Autowired
+    public UserHandler (final UserService userService) {
+        this.userService = userService;
+    }
+
+    /**
+     * 获取全部用户列表
+     *
+     * @param request 请求
+     * @return 响应
+     */
+    public Mono<ServerResponse> listAll (final ServerRequest request) {
+        return ServerResponse
+            .ok()
+            .body(userService.listAll(), User.class);
+    }
 }
