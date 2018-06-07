82e40dad536865954734b6fe13b3d2d0c73cdabe
cutiechi <chi@cutie.ooo>
2018-06-07 08:37:27 +0800

    feat(service): add user service interface and impl

---
+++ src/main/java/com/cutiechi/demo/service/UserService.java
@@ -0,0 +1,10 @@
+package com.cutiechi.demo.service;
+
+/**
+ * 用户业务逻辑接口
+ *
+ * @author Cutie Chi
+ */
+public interface UserService {
+
+}
---
+++ src/main/java/com/cutiechi/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.cutiechi.demo.service.impl;
+
+import com.cutiechi.demo.service.UserService;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 用户业务逻辑实现类
+ *
+ * @author Cutie Chi
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+}
