efa265416e425df10a309cdab517f01aab06a3dc
cutiechi <chi@cutie.ooo>
2018-06-07 09:19:54 +0800

    feat(config): add insert user router config

--- src/main/java/com/cutiechi/demo/config/RouterConfig.java
+++ src/main/java/com/cutiechi/demo/config/RouterConfig.java
@@ -10,9 +10,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.http.MediaType.TEXT_PLAIN;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
@@ -38,6 +40,11 @@ public class RouterConfig {
             // 获取全部用户列表, API 为 GET 请求 /users
             GET("/users").and(accept(TEXT_PLAIN)),
             userHandler::listAll
+        ).andRoute(
+
+            // 添加用户, API 为 POST 请求 /users
+            POST("/users").and(accept(APPLICATION_JSON)),
+            userHandler::insert
         );
     }
 }
