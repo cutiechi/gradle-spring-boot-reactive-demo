6cf0b5f2e7ee5878a1bb79664d36a5bbb3f14e43
cutiechi <chi@cutie.ooo>
2018-06-07 09:11:40 +0800

    feat(config): add list all user router config

--- src/main/java/com/cutiechi/demo/config/RouterConfig.java
+++ src/main/java/com/cutiechi/demo/config/RouterConfig.java
@@ -1,6 +1,21 @@
 package com.cutiechi.demo.config;
 
+import com.cutiechi.demo.web.handler.UserHandler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.http.MediaType.TEXT_PLAIN;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
 
 /**
  * 路由配置类
@@ -10,4 +25,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RouterConfig {
 
+    /**
+     * 用户路由
+     *
+     * @param userHandler 用户处理对象
+     * @return 路由函数
+     */
+    @Bean
+    public RouterFunction<ServerResponse> userRouter (@Autowired UserHandler userHandler) {
+        return route(
+
+            // 获取全部用户列表, API 为 GET 请求 /users
+            GET("/users").and(accept(TEXT_PLAIN)),
+            userHandler::listAll
+        );
+    }
 }
